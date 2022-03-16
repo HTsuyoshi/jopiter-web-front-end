9c23044287057b478fe313ab2dce52edfbb1f454
HTsuyoshi <henriquetsuyoshiy@gmail.com>
2022-03-16 00:04:22 -0300

    a

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,8 +4,7 @@ const renderHome = async (req, res, next) => {
   const response = await fetch('http://localhost:3001/test/1');
 
   const body = await response.json();
-  res.render('index2', { title: 'Jopiter Classificador Web', message: JSON.stringify(body), algumaVariavel: 'lalalala'});
-  //res.render('index', { title: 'Jopiter Classificador Web', message: JSON.stringify(body), algumaVariavel: 'lalalala'});
+  res.render('index', { title: 'Jopiter Classificador Web', message: JSON.stringify(body), algumaVariavel: 'lalalala'});
 }
 
 module.exports = {
