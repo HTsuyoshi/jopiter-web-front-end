37d5636c9e00b0cd1f816ec72a9a9af3c634d1c9
HTsuyoshi <henriquetsuyoshiy@gmail.com>
2022-04-07 01:28:28 -0300

    a

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,8 @@
+const config = require('config');
 const fetch = require('node-fetch');
+
+//const BACKEND_URL = config.get('jopiter-backend.url');
+//const BACKEND_URL = config.get('localhost:3000');
 
 const renderHome = async (req, res, next) => {
   const response = await fetch('http://localhost:3001/test/1');
